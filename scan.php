53025bd0a967f55c7d54d33b371462ad761cc337
Николай Торгов <ntorgov@gmail.com>
2016-03-05 15:51:35 +0300

    KDSH-3 KDSH-9 KDSH-10 KDSH-11 #comment По идее, должно сканироваться

--- scan.php
+++ scan.php
@@ -17,10 +17,10 @@ class Scanner {
 
 	private $realFileName = null;
 
-	static $scanResults = array();
+	private $scanResults = array();
 
 	function __construct() {
-		self::$scanResults = array();
+		$this->scanResults = array();
 		$this->SignaturesDir = 'http://thekadeshi.bagdad.tmweb.ru/signatures';
 		//$this->GetSignaturesFiles();
 		$this->LoadRules();
@@ -50,6 +50,8 @@ class Scanner {
 	 * Функция загрузки правил определения CMS
 	 *
 	 * @return array
+	 *
+	 * @todo На какое-то время это можно убрать.
 	 */
 	private function LoadRules() {
 		$rules = array();
@@ -78,12 +80,18 @@ class Scanner {
 	}
 
 	public function Scan($fileName) {
+		$this->scanResults = array();
 		$content = $this->GetFileContent($fileName);
 		$this->ScanContent($content);
 
-		return (!empty(self::$scanResults))?self::$scanResults:null;
+		return (!empty($this->scanResults))?$this->scanResults:null;
 	}
 
+	/**
+	 * Функция получения содержимого файла
+	 * @param $fileName
+	 * @return string
+	 */
 	private function GetFileContent($fileName) {
 		$content = file_get_contents($fileName);
 		$this->realFileName = pathinfo($fileName);
@@ -95,17 +103,63 @@ class Scanner {
 		foreach($this->Signatures as $virusSignature) {
 
 			preg_match($virusSignature['signature'], $content, $results);
-			if(!empty($results)) {
+
+			if(isset($results) && !empty($results)) {
+				//print_r($results);
 				$files[] = array('file'=>'', 'action'=>$virusSignature['action']);
-				self::$scanResults[] = array(
+				$this->scanResults = array(
 					'file' => $this->realFileName,
 				    'name' => $virusSignature['name'],
-					'content' => $results[0],
+					'positions' => array(
+						'start' => mb_strpos($content, $results[0]),
+						'length' => mb_strlen($results[0])
+					),
+					//'content' => $results[0],
 					'action' => $virusSignature['action']
 				);
 			}
 			$content = preg_replace($virusSignature['signature'], '', $content);
 
+		}
+	}
+}
+
+class Healer {
+
+	public $Anamnesis;
+
+	function __construct()
+	{
+		$this->Anamnesis = array();
+		$this->GetAnamnesis();
+		if(!empty($this->Anamnesis)) {
+			//cure
+		}
+	}
+
+	public function GetAnamnesis() {
+		$anamnesisContent = file_get_contents('kadeshi.anamnesis.json');
+		$this->Anamnesis = json_decode($anamnesisContent, true);
+	}
+
+	public function Cure($infectedElement) {
+		$filePath = $infectedElement['file']['dirname'] . '/' . $infectedElement['file']['basename'];
+		$cureAction = $infectedElement['action'];
+		switch(mb_strtolower($cureAction)){
+			case 'delete':
+				$unlinkResult = unlink($filePath);
+				// @todo Поставить полноценную проверку на удаление
+				if($unlinkResult === false) {
+					chmod($filePath, 0600);
+					unlink($filePath);
+				}
+				break;
+			case 'cure':
+				$fileContent = file_get_contents($filePath);
+				$fileParts[0] = mb_substr($fileContent, 0, $infectedElement['positions']['start']);
+				$fileParts[1] = mb_substr($fileContent, $infectedElement['positions']['start'] + $infectedElement['positions']['length']);
+				file_put_contents($filePath, $fileParts[0] . $fileParts[1]);
+				break;
 		}
 	}
 }
@@ -135,20 +189,92 @@ class FileList {
 	}
 }
 
-$scanner = new Scanner();
-$filelist = new FileList();
+/**
+ * Класс для работы с консолью
+ */
+class Console {
 
-$filelist->GetFileList(__DIR__);
+	/**
+	 * Цвет текста в консоли
+	 * @var array
+	 */
+	public $Color;
+
+	/**
+	 * Флаг, требуется ли вывод
+	 * @var boolean
+	 */
+	private $IsVerbose;
+
+	/**
+	 * Console constructor.
+	 * @param $Verbose boolean
+	 */
+	function __construct($Verbose) {
+
+		$this->IsVerbose = $Verbose;
+
+		$this->Color = array(
+			'green' => chr(27) . "[30;32m",
+			'red' => chr(27) . "[30;31m",
+			'normal' => chr(27) . "[0m",
+		);
+	}
+
+	/**
+	 * Функция вывода текста в консоль
+	 * @param $Message string
+	 */
+	public function Log($Message) {
+		if($this->IsVerbose === true) {
+			echo($Message . "\r\n");
+		}
+	}
+}
+
+
+
+if($argc > 1) {
+	foreach ($argv as $argument) {
+		if (strtolower($argument) == '--scan') {
+			$currentAction = 'scan';
+		}
+		if(strtolower($argument) == '--verbose') {
+			if(!defined('VERBOSE')) {
+				define('VERBOSE', true);
+			}
+		}
+	}
+} else {
+	$currentAction = 'scan';
+}
+
+$Console = new Console(defined('VERBOSE')?VERBOSE:false);
+
+if($currentAction == 'scan' || $currentAction == null) {
+
+	$Console->Log("Current action: " . $Console->Color['green'] . "Scanning" . $Console->Color['normal'] );
+
+	$scanResults = array();
+	$scanner = new Scanner();
+	$filelist = new FileList();
+
+	$filelist->GetFileList(__DIR__);
 
 //print_r($filelist->fileList);
 
-foreach($filelist->fileList as $file) {
+	foreach ($filelist->fileList as $file) {
 
-	$scanResults = $scanner->Scan($file);
-	if($scanResults != null) {
-		$results[] = $scanResults;
+		$fileScanResults = $scanner->Scan($file);
+		if ($fileScanResults != null) {
+			$scanResults[] = $fileScanResults;
+
+			$Console->Log($fileScanResults['file']['dirname'] . '/' . $fileScanResults['file']['basename'] . ' infection: ' . $Console->Color['red'] . $fileScanResults['name'] . $Console->Color['normal'] );
+		}
 	}
-
+	if(!empty($scanResults)) {
+		//for
+		$encodedResults = json_encode($scanResults);
+		$resultsFile = file_put_contents("kadeshi.anamnesis.json", $encodedResults);
+	}
 }
-
-print_r($results);
