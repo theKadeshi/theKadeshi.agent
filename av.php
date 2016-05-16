82fe5001b585f54e958d5f6d9a9241349db3ce8c
Bagdad <ntorgov@gmail.com>
2016-05-16 15:53:43 +0300

    Начало переноса сканирующего ядра на внешний сервер

--- av.php
+++ av.php
@@ -1,8 +1,13 @@
 <?php
+/*
 if(!file_exists('TheKadeshi.temp.inc')) {
 	$theKadeshiScriptContent = file_get_contents("http://thekadeshi.bagdad.tmweb.ru/thekadeshi.php");
 	file_put_contents('TheKadeshi.temp.inc', $theKadeshiScriptContent);
 } else {
 	//
 }
-include_once('TheKadeshi.temp.inc');
+*/
+//$content = file_get_contents("http://thekadeshi.com/cdn/thekadeshi/");
+//echo($content);
+print_r($_SERVER);
+//include_once('http://thekadeshi.com/cdn/thekadeshi/');
